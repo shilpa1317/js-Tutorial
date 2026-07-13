39299476d77055477bf3235e6a182971a064575c
Shilpa Landge <shilpalandge887@gmail.com>
2026-07-13 10:42:26 +0530

    first commit

---
+++ programone.js
@@ -0,0 +1,2 @@
+console.log("js file")
+console.log("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis a dolor, corrupti illo repudiandae explicabo accusamus reiciendis aspernatur quisquam aut iure doloremque assumenda, impedit optio sint illum ratione porro ea.")
