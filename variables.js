763d7dd2c39d669996bd2e6a3438538b36df2bd6
Shilpa Landge <shilpalandge887@gmail.com>
2026-07-14 17:03:34 +0530

    added variables.js

---
+++ variables.js
@@ -0,0 +1,24 @@
+let age=21;
+let temp= -5;
+let name = 'shilpa';
+let message = 'hello , good morining ,nice to meet you ';
+let issunny=true;
+let car={
+    color:'red',
+    wheels:4,
+
+
+};
+
+let fruits=["orange","apple","mango","banana"];
+
+let car;
+
+let lostcar=null;
+
+let stars=1223243474356387558604n;
+
+let sec1=symbol("ram1")
+let sec2=symbol("ram1")
+
+
